aa24622ed26966da7752a50d6789388d6c6de9dc
ankurraina66 <65275253+ankurraina66@users.noreply.github.com>
2026-04-10 11:57:26 +0530

    Update ConsultantServiceImpl.java

--- src/main/java/com/levent/consultantapi/service/consultant/impl/ConsultantServiceImpl.java
+++ src/main/java/com/levent/consultantapi/service/consultant/impl/ConsultantServiceImpl.java
@@ -17,6 +17,8 @@ import com.levent.consultantapi.service.ConsultantService;
 public class ConsultantServiceImpl implements ConsultantService {
 
 	String password = "sdfds";
+	String username = "dummy";
+	
 	@Autowired
 	private ConsultantRepository consultantRepository;
 
